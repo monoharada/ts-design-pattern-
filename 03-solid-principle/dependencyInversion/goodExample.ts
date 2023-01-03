fc23af934e0977cb07712f104e800b6777e6c601
monoharada <harada@monosus.co.jp>
2023-01-04 08:08:54 +0900

    feat: 🎸 learn depandency inversion
    BREAKING CHANGE: 🧨 no

--- 03-solid-principle/dependencyInversion/goodExample.ts
+++ 03-solid-principle/dependencyInversion/goodExample.ts
@@ -1,74 +1,77 @@
-export {}
+export {};
 
 class User {}
 
 interface IUserService {
-    create(user: User): User;
-    findById(id: string): User;
+  create(user: User): User;
+  findById(id: string): User;
 }
 
 class UserController {
-    constructor(private userService: IUserService) {
-        this.userService = userService;
-    }
+  //   private userService = new UserService();
+  constructor(private userService: IUserService) {
+    this.userService = userService;
+  }
 
-    create(user: User): User {
-        return this.userService.create(user);
-    }
+  create(user: User): User {
+    return this.userService.create(user);
+  }
 
-    findById(id: string): User {
-        return this.userService.findById(id);
-    }
+  findById(id: string): User {
+    return this.userService.findById(id);
+  }
 }
 
 interface IUserRepository {
-    create(user: User): User;
-    findById(id: string): User;
+  create(user: User): User;
+  findById(id: string): User;
 }
 
 class UserService implements IUserService {
-    constructor(private userRepository: IUserRepository) {
-        this.userRepository = userRepository;
-    }
-    create(user: User): User {
-        return this.userRepository.create(user);
-    }
+  //   private userRepository = new UserRdbRepository();
+  constructor(private userRepository: IUserRepository) {
+    this.userRepository = userRepository;
+  }
 
-    findById(id: string): User {
-        return this.userRepository.findById(id);
-    }
+  create(user: User): User {
+    return this.userRepository.create(user);
+  }
+
+  findById(id: string): User {
+    return this.userRepository.findById(id);
+  }
 }
 
 class UserRdbRepository implements IUserRepository {
-    create(user: User): User {
-        console.log("RDBにUserを登録");
-        return user;
-    }
+  create(user: User): User {
+    console.log('RDBにUserを登録');
+    return user;
+  }
 
-    findById(id: string): User {
-        console.log(`ID: ${id}のユーザーを検索`);
-        return new User();
-    }
+  findById(id: string): User {
+    console.log(`ID: ${id}のユーザーを検索`);
+    return new User();
+  }
 }
 
-class TestRepository implements IUserRepository {
-    create(user: User): User {
-        console.log("RDBにUserを登録");
-        return user;
-    }
+class TestUserRdbRepository implements IUserRepository {
+  create(user: User): User {
+    console.log('RDBにUserを登録');
+    return user;
+  }
 
-    findById(id: string): User {
-        console.log(`Test ID: ${id}のユーザーを検索`);
-        return new User();
-    }
+  findById(id: string): User {
+    console.log(`Test ID: ${id}のユーザーを検索`);
+    return new User();
+  }
 }
 
 function run() {
-    // const repository = new UserRdbRepository();
-    const repository = new TestRepository();
-    const service = new UserService(repository);
-    const userController = new UserController(service);
-    userController.findById("123");
+  //   const repository = new UserRdbRepository();
+  const repository = new TestUserRdbRepository();
+  const service = new UserService(repository);
+  const userController = new UserController(service);
+  userController.findById('123');
 }
 
 run();
