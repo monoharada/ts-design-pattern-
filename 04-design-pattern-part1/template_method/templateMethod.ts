1b3e292bb2728763342bd21ca0e7fa9608430c07
monoharada <harada@monosus.co.jp>
2023-01-05 08:25:24 +0900

    feat: 🎸 learn template method

--- 04-design-pattern-part1/template_method/templateMethod.ts
+++ 04-design-pattern-part1/template_method/templateMethod.ts
@@ -1,47 +1,43 @@
-export {}
+export {};
 
 abstract class TestTemplate {
-    test() {
-        this.setup();
-        this.execute();
-        this.teardown();
-    }
+  test() {
+    this.setup();
+    this.execute();
+    this.teardown();
+  }
 
-    abstract setup();
-    abstract execute();
+  abstract setup();
+  abstract execute();
 
-    teardown() {
-        console.log("teardown");
-    }
+  teardown() {
+    console.log('teardown');
+  }
 }
 
 class ItemServiceTest extends TestTemplate {
-    setup() {
-        console.log("setup: ItemServiceTest");
-    }
-
-    execute() {
-        console.log("execute: ItemServiceTest");
-    }
+  setup() {
+    console.log('setup: ItemServiceTest');
+  }
+  execute() {
+    console.log('execute: ItemServiceTest');
+  }
 }
-
 class UserServiceTest extends TestTemplate {
-    setup() {
-        console.log("setup: UserServiceTest");
-    }
-
-    execute() {
-        console.log("execute: UserServiceTest");
-    }
+  setup() {
+    console.log('setup: UserServiceTest');
+  }
+  execute() {
+    console.log('execute: UserServiceTest');
+  }
 }
-
 function run() {
-    const itemServiceTest = new ItemServiceTest();
-    const userServiceTest = new UserServiceTest();
+  const itemServiceTest = new ItemServiceTest();
+  const userServiceTest = new UserServiceTest();
 
-    itemServiceTest.test();
-    console.log("");
-    userServiceTest.test();
+  itemServiceTest.test();
+  console.log('');
+  userServiceTest.test();
 }
 
 run();
