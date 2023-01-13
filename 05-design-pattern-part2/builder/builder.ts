8b4379089b5bdf673ab7de314a01d29020464bd8
monoharada <harada@monosus.co.jp>
2023-01-13 09:01:03 +0900

    feat: 🎸 learn Builder pattern

--- 05-design-pattern-part2/builder/builder.ts
+++ 05-design-pattern-part2/builder/builder.ts
@@ -1,84 +1,73 @@
 export {};
 
 class Computer {
-    type: string;
-    cpu: string;
-    ram: number;
+  type: string;
+  cpu: string;
+  rum: number;
 }
-
 interface ComputerBuilder {
-    addCpu(cpu: string);
-    addRam(ram: number);
+  addCpu(cpu: string): void;
+  addRam(ram: number): void;
 }
 
 class DesktopBuilder implements ComputerBuilder {
-    private computer: Computer;
+  private computer: Computer;
 
-    constructor() {
-        this.computer = new Computer();
-        this.computer.type = "Desktop";
-    }
-
-    addCpu(cpu: string) {
-        this.computer.cpu = cpu;
-    }
-
-    addRam(ram: number) {
-        this.computer.ram = ram;
-    }
-
-    getResult(): Computer {
-        return this.computer;
-    }
+  constructor() {
+    this.computer = new Computer();
+    this.computer.type = 'DeskTop';
+  }
+  addCpu(cpu: string) {
+    this.computer.cpu = cpu;
+  }
+  addRam(ram: number) {
+    this.computer.rum = ram;
+  }
+  getResult(): Computer {
+    return this.computer;
+  }
 }
-
 class LaptopBuilder implements ComputerBuilder {
-    private computer: Computer;
+  private computer: Computer;
 
-    constructor() {
-        this.computer = new Computer();
-        this.computer.type = "Laptop";
-    }
-
-    addCpu(cpu: string) {
-        this.computer.cpu = cpu;
-    }
-
-    addRam(ram: number) {
-        this.computer.ram = ram;
-    }
-
-    getResult(): Computer {
-        return this.computer;
-    }
+  constructor() {
+    this.computer = new Computer();
+    this.computer.type = 'LapTop';
+  }
+  addCpu(cpu: string) {
+    this.computer.cpu = cpu;
+  }
+  addRam(ram: number) {
+    this.computer.rum = ram;
+  }
+  getResult(): Computer {
+    return this.computer;
+  }
 }
-
 class Director {
-    constructor(private builder: ComputerBuilder) {}
+  constructor(private builder: ComputerBuilder) {}
 
-    construct() {
-        this.builder.addCpu("Core i5");
-        this.builder.addRam(16);
-    }
-
-    highSpecConstruct() {
-        this.builder.addCpu("Core i9");
-        this.builder.addRam(64);
-    }
+  construct() {
+    this.builder.addCpu('Core i5');
+    this.builder.addRam(16);
+  }
+  highSpecConstruct() {
+    this.builder.addCpu('Core i9');
+    this.builder.addRam(64);
+  }
 }
 
 function run() {
-    const desktopBuilder = new DesktopBuilder();
-    const desktopDirector = new Director(desktopBuilder);
-    desktopDirector.construct();
-    const desktopComputer = desktopBuilder.getResult();
-    console.log(desktopComputer);
+  const desktopBuilder = new DesktopBuilder();
+  const desktopDirector = new Director(desktopBuilder);
+  desktopDirector.construct();
+  const desktopComputer = desktopBuilder.getResult();
+  console.log(desktopComputer);
 
-    const laptopBuilder = new LaptopBuilder();
-    const laptopDirector = new Director(laptopBuilder);
-    laptopDirector.highSpecConstruct();
-    const laptopComputer = laptopBuilder.getResult();
-    console.log(laptopComputer);
+  const laptopBuilder = new LaptopBuilder();
+  const laptopDirector = new Director(laptopBuilder);
+  laptopDirector.highSpecConstruct();
+  const laptopComputer = laptopBuilder.getResult();
+  console.log(laptopComputer);
 }
-
 run();
